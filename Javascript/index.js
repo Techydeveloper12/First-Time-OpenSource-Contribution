12b17eaaa3179553f57b44796bc0496c6f38c6e6
bharat <bharatveeryadavg@gmail.com>
2022-09-20 23:12:44 +0530

    project

---
+++ Javascript/index.js
@@ -0,0 +1,44 @@
+// How to use of Expressoin
+
+// const country = "France";
+// console.log(`my name is ${country}`);
+// const x = 5;
+// const y = 6;
+// console.log(`sum of two no is : ${x + y} + ${y}`)   
+
+
+// //Type Conversions
+// function checkThis () {
+//     console.log(this)
+// }
+
+
+// let obj ={
+//     a : 18,
+//     b : 48,
+//     c : 34,
+//     d : "baharat",
+//     f : function   (){
+//         console.log(this)
+//     },
+//      bharat: "bharat",
+//     e : {
+//         p : "veer",
+//         q : 9568,
+        
+//     },
+
+//         z : function () {
+//             console.log(this)
+//         },
+
+
+
+
+// }
+
+
+
+
+
+     
---
+++ Javascript/script.js
@@ -0,0 +1,23 @@
+let s1 = 'he said , "this is awesome!"'
+
+let s2 = "he said, \"This is is awesome!\""
+
+let s3 = "he said, 'thsi is is awesome'"
+
+let s4 = `"he said, 'thsi is is awesome'`
+
+let s5 = `"he said, 'thsi \nis is awesome'`
+
+
+
+
+
+
+
+
+
+
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
