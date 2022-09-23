bafdbfd0be58ddaa43df26002ece7e78d8285a0f
bharat <bharatveeryadavg@gmail.com>
2022-09-23 23:02:55 +0530

    upda

---
+++ script.js
@@ -0,0 +1,108 @@
+// const computerChoiceDisplay = document.getElementById('computer-choice')
+// const userChoiceDisplay = document.getElementById('user-choice')
+// const resultDisplay = document.getElementById('result')
+// const possibleChoices = document.querySelectorAll('button')
+// let userChoice
+// let computerChoice
+// let result
+
+// possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+//   userChoice = e.target.id
+//   userChoiceDisplay.innerHTML = userChoice
+//   generateComputerChoice()
+//   getResult()
+// }))
+
+// function generateComputerChoice() {
+//   const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+  
+//   if (randomNumber === 1) {
+//     computerChoice = 'rock'
+//   }
+//   if (randomNumber === 2) {
+//     computerChoice = 'scissors'
+//   }
+//   if (randomNumber === 3) {
+//     computerChoice = 'paper'
+//   }
+//   computerChoiceDisplay.innerHTML = computerChoice
+// }
+
+// function getResult() {
+//   if (computerChoice === userChoice) {
+//     result = 'its a draw!'
+//   }
+//   if (computerChoice === 'rock' && userChoice === "paper") {
+//     result = 'you win!'
+//   }
+//   if (computerChoice === 'rock' && userChoice === "scissors") {
+//     result = 'you lost!'
+//   }
+//   if (computerChoice === 'paper' && userChoice === "scissors") {
+//     result = 'you win!'
+//   }
+//   if (computerChoice === 'paper' && userChoice === "rock") {
+//     result = 'you lose!'
+//   }
+//   if (computerChoice === 'scissors' && userChoice === "rock") {
+//     result = 'you win!'
+//   }
+//   if (computerChoice === 'scissors' && userChoice === "paper") {
+//     result = 'you lose!'
+//   }
+//   resultDisplay.innerHTML = result
+// }
+
+
+
+
+// let a = 10
+// function f1(x) {
+//     return 2 * x +3
+
+// }
+
+// f1(10)
+// 
+
+// let f1 = (x) => (2 * x + 3);
+
+// console.log(f1(11))
+
+
+// let sum = (a,b) => a+b
+
+// console.log(sum(5,4));
+// let mul = (a,b) => a*b;
+// console.log(mul(5,4))
+
+
+
+
+
+let message = "hello";
+message = 123456;
+
+
+
+
+console.log(message)
+
+
+
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
