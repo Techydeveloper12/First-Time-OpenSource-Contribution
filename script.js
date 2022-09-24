e836ba663fca2657844cc8bfb661c2e6b430101c
bharat <bharatveeryadavg@gmail.com>
2022-09-24 10:54:17 +0530

    passbyvalue-passbyreference

--- script.js
+++ script.js
@@ -80,17 +80,52 @@
 
 
 
-let message = "hello";
-message = 123456;
+// let message = "hello";
+// message = 123456;
 
 
 
 
-console.log(message)
+// console.log(message)
 
 
 
-console.log(typeof null);
+// console.log(typeof null);
+
+
+
+// let likes = 100;
+
+const youtube = {
+    likes : 100,
+    name : "bharat",
+    subscribers : 500
+}
+const you = {
+    likes : 100,
+}
+
+const veer = (a,b) => {
+    a.likes = 200;
+    b.subscribers = 1000;
+}
+
+
+veer(you, youtube);
+
+console.log(you.likes);
+console.log(youtube.subscribers);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
